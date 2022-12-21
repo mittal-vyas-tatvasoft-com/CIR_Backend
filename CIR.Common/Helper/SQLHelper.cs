a3836ffd7c325b1fd05de62bf5da873d2d5683a8
Trusha <trusha.kalariya@tatvasoft.com>
2022-12-21 16:40:13 +0530

    initial commit

---
+++ CIR.Common/Helper/SQLHelper.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using System.Dynamic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AMPCore.Data.Helper
+{
+    public static class SQLHelper
+    {
+        //public static IEnumerable<dynamic> DynamicListFromSql(this DbContext db, string Sql, Dictionary<string, object> Params)
+        //{
+        //    using (var cmd = db.Database.GetDbConnection().CreateCommand())
+        //    {
+        //        cmd.CommandText = Sql;
+        //        cmd.CommandType = CommandType.StoredProcedure;
+        //        if (cmd.Connection.State != ConnectionState.Open) { cmd.Connection.Open(); }
+
+        //        foreach (KeyValuePair<string, object> p in Params)
+        //        {
+        //            DbParameter dbParameter = cmd.CreateParameter();
+        //            dbParameter.ParameterName = p.Key;
+        //            dbParameter.Value = p.Value;
+        //            cmd.Parameters.Add(dbParameter);
+        //        }
+
+        //        using (var dataReader = cmd.ExecuteReader())
+        //        {
+        //            while (dataReader.Read())
+        //            {
+        //                var row = new ExpandoObject() as IDictionary<string, object>;
+        //                for (var fieldCount = 0; fieldCount < dataReader.FieldCount; fieldCount++)
+        //                {
+        //                    row.Add(dataReader.GetName(fieldCount), dataReader[fieldCount]);
+        //                }
+        //                yield return row;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public static DataSet DynamicListFromSqlGrid(this DbContext db, string Sql, Dictionary<string, object> Params)
+        //{
+        //    DataSet ds = new DataSet();
+        //    try
+        //    {
+
+        //        IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+        //        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        //        using (SqlConnection sqlConn = new SqlConnection(connectionString))
+        //        {
+        //            // string sql = "GetDataByFields";
+        //            //List<SqlParameter param = new SqlParameter();
+        //            using (SqlCommand sqlCmd = new SqlCommand(Sql, sqlConn))
+        //            {
+        //                sqlCmd.CommandType = CommandType.StoredProcedure;
+        //                foreach (KeyValuePair<string, object> p in Params)
+        //                {
+        //                    //DbParameter dbParameter = sqlCmd.CreateParameter();
+        //                    //dbParameter.ParameterName = p.Key;
+        //                    //dbParameter.Value = p.Value;
+        //                    //sqlCmd.Parameters.Add(dbParameter);
+
+        //                    sqlCmd.Parameters.AddWithValue(p.Key, p.Value);
+        //                }
+        //                sqlCmd.CommandTimeout = 0;
+        //                sqlConn.Open();
+        //                using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlCmd))
+        //                {
+        //                    sqlAdapter.Fill(ds);
+        //                }
+        //                sqlConn.Close();
+        //                SqlConnection.ClearPool(sqlConn);
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw ex;
+        //    }
+
+        //    return ds;
+        //}
+
+        //public static List<T> RawSqlQuery<T>(AmpCoreContext context, string query, Func<DbDataReader, T> map)
+        //{
+        //    using (var command = context.Database.GetDbConnection().CreateCommand())
+        //    {
+        //        command.CommandText = query;
+        //        command.CommandType = CommandType.Text;
+
+        //        context.Database.OpenConnection();
+
+        //        using (var result = command.ExecuteReader())
+        //        {
+        //            var entities = new List<T>();
+
+        //            while (result.Read())
+        //            {
+        //                entities.Add(map(result));
+        //            }
+
+        //            return entities;
+        //        }
+        //    }
+        //}
+
+        //public static object ExecuteScalarCommand(AmpCoreContext context, string query, List<SqlParameter> parameters)
+        //{
+        //    object retValue;
+        //    using (var command = context.Database.GetDbConnection().CreateCommand())
+        //    {
+        //        command.CommandText = query;
+        //        command.CommandType = CommandType.Text;
+        //        if (parameters != null)
+        //        {
+        //            command.Parameters.AddRange(parameters.ToArray());
+        //        }
+        //        context.Database.OpenConnection();
+
+        //        retValue = command.ExecuteScalar();
+        //    }
+        //    return retValue;
+        //}
+
+        //public static DataTable ExecuteSqlQuery(string Sql)
+        //{
+        //    DataTable dt = new DataTable();
+        //    try
+        //    {
+        //        IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+        //        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        //        using (SqlConnection sqlConn = new SqlConnection(connectionString))
+        //        {
+        //            SqlCommand cmd = new SqlCommand(Sql, sqlConn);
+        //            cmd.CommandType = CommandType.Text;
+        //            sqlConn.Open();
+        //            SqlDataAdapter da = new SqlDataAdapter();
+        //            da.SelectCommand = cmd;
+        //            da.Fill(dt);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw ex;
+        //    }
+        //    return dt;
+        //}
+
+       
+    }
+}
---
+++ CIR.Models/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CIR.Models
+{
+    public class Class1
+    {
+
+    }
+}
