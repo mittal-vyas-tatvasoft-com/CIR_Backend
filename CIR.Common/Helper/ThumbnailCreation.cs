7e8fcdebb553c1ac79a63874232d053251457942
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2023-01-09 18:18:02 +0530

    CIR-17 New Thumbnail Creation

---
+++ CIR.Common/Helper/ThumbnailCreation.cs
@@ -0,0 +1,37 @@
+﻿using CIR.Common.CommonModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Drawing;
+
+namespace CIR.Common.Helper
+{
+    public class ThumbnailCreation
+    {
+        private readonly ThumbnailModel _thumbnailModel;
+        public ThumbnailCreation(IOptions<ThumbnailModel> thumbnailModel)
+        {
+            _thumbnailModel = thumbnailModel.Value;
+        }
+
+        public void CreateThumbnailImage(IFormFile actualImage)
+        {
+            try
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                actualImage.CopyTo(memoryStream);
+
+                using (Image image = Image.FromStream(memoryStream))
+                {
+                    int thumbnailImageHeight = Convert.ToInt32(_thumbnailModel.ScalingFactor * image.Height);
+                    int thumbnailImageWidth = Convert.ToInt32(_thumbnailModel.ScalingFactor * image.Width);
+
+                    var thumbnailImage = image.GetThumbnailImage(thumbnailImageHeight, thumbnailImageWidth, () => false, IntPtr.Zero);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
