28bc04382f3302476d7d4e6d8dd7382681021461
ChiragMajethiyaTatvasoft <121600915+ChiragMajethiyaTatvasoft@users.noreply.github.com>
2023-01-10 11:24:37 +0530

    merged with main and error resolution

--- CIR.Common/Data/CIRDbContext.cs
+++ CIR.Common/Data/CIRDbContext.cs
@@ -30,9 +30,10 @@ namespace CIR.Common.Data
 		}
 		public DbSet<Roles> Roles { get; set; }
 		public DbSet<Culture> Cultures { get; set; }
-        public DbSet<GlobalConfigurationCutOffTime> GlobalConfigurationCutOffTimes { get; set; }
+		public DbSet<GlobalConfigurationCutOffTime> GlobalConfigurationCutOffTimes { get; set; }
 
-    }
 		public DbSet<Holidays> Holidays { get; set; }
+		public DbSet<Fonts> Fonts { get; set; }
 	}
 }
+
---
+++ CIR.Data/Data/GlobalConfig/FontRepository.cs
@@ -0,0 +1,30 @@
+﻿using CIR.Common.Data;
+using CIR.Core.Interfaces.GlobalConfig;
+
+namespace CIR.Data.Data.GlobalConfig
+{
+	public class FontRepository : IFontRepository
+	{
+		#region PROPERTIES
+		private readonly CIRDbContext _CIRDBContext;
+		#endregion
+
+		#region CONSTRUCTOR
+		public FontRepository(CIRDbContext context)
+		{
+			_CIRDBContext = context ??
+				throw new ArgumentNullException(nameof(context));
+		}
+		#endregion
+
+		#region METHODS
+
+		//public async Task<Fonts> GetAllFonts()
+		//{
+		//	var fonts = await _CIRDBContext.Fonts
+		//}
+		#endregion
+
+
+	}
+}
