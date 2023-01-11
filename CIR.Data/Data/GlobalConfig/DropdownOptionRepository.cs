c3069236bedfc706f7da68f07a5b6818553fa203
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2023-01-11 18:57:02 +0530

    Dapper implemeted for Get all dropdown options

--- CIR.Data/Data/GlobalConfig/DropdownOptionRepository.cs
+++ CIR.Data/Data/GlobalConfig/DropdownOptionRepository.cs
@@ -4,7 +4,6 @@ using CIR.Core.Entities.GlobalConfig;
 using CIR.Core.Interfaces.GlobalConfig;
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Data;
 
 namespace CIR.Data.Data.GlobalConfig
@@ -35,23 +34,23 @@ namespace CIR.Data.Data.GlobalConfig
         /// <returns>list of global congig reasons</returns>
         public async Task<IActionResult> GetAllDropdownOptions()
         {
-
-            var globalConfigReasons = await (from globalCongigReason in _CIRDbContext.GlobalConfigurationReasons
-                                             select new GlobalConfigurationReasons()
-                                             {
-                                                 Id = globalCongigReason.Id,
-                                                 Type = globalCongigReason.Type,
-                                                 Enabled = globalCongigReason.Enabled,
-                                                 Content = globalCongigReason.Content,
-                                             }).ToListAsync();
-
-            if (globalConfigReasons.Count > 0)
+            using (DbConnection dbConnection = new DbConnection())
             {
-                return new JsonResult(new CustomResponse<List<GlobalConfigurationReasons>>() { StatusCode = (int)HttpStatusCodes.Success, Result = true, Message = HttpStatusCodesMessages.Success, Data = globalConfigReasons });
-            }
-            else
-            {
-                return new JsonResult(new CustomResponse<string>() { StatusCode = (int)HttpStatusCodes.NoContent, Result = true, Message = HttpStatusCodesMessages.NoContent, Data = "No Data is present" });
+
+                using (var connection = dbConnection.Connection)
+                {
+                    var globalConfigReasons = await connection.QueryMultipleAsync("spGetAllDropdownOptions", null, commandType: CommandType.StoredProcedure);
+                    var dropdownOptions = globalConfigReasons.Read<GlobalConfigurationReasons>().ToList();
+
+                    if (dropdownOptions.Count > 0)
+                    {
+                        return new JsonResult(new CustomResponse<List<GlobalConfigurationReasons>>() { StatusCode = (int)HttpStatusCodes.Success, Result = true, Message = HttpStatusCodesMessages.Success, Data = dropdownOptions });
+                    }
+                    else
+                    {
+                        return new JsonResult(new CustomResponse<string>() { StatusCode = (int)HttpStatusCodes.NoContent, Result = true, Message = HttpStatusCodesMessages.NoContent, Data = "No Data is present" });
+                    }
+                }
             }
 
         }
@@ -74,7 +73,6 @@ namespace CIR.Data.Data.GlobalConfig
                         return new JsonResult(new CustomResponse<string>() { StatusCode = (int)HttpStatusCodes.BadRequest, Result = false, Message = HttpStatusCodesMessages.BadRequest, Data = "Error occurred while adding new Global Currency" });
 
                     }
-                    string result;
                     using (DbConnection dbConnection = new DbConnection())
                     {
 
@@ -86,8 +84,7 @@ namespace CIR.Data.Data.GlobalConfig
                             parameters.Add("@Enabled", option.Enabled);
                             parameters.Add("@Content", option.Content);
 
-                            var DataSet = connection.QueryMultiple("spAddorUpdateDropDownOptions", parameters, commandType: CommandType.StoredProcedure);
-                            result = DataSet.Read<string>().FirstOrDefault();
+                            connection.QueryMultiple("spAddorUpdateDropDownOptions", parameters, commandType: CommandType.StoredProcedure);
                         }
                     }
                 }
