6e7e2d49c68ac7cee5eff49b7e08f56de3af717b
GohilAmit <114397099+GohilAmit@users.noreply.github.com>
2023-01-12 16:08:28 +0530

    GlobalConfiguration fonts changes

---
+++ CIR.Application/Services/GlobalConfig/GlobalConfigurationFontsServices.cs
@@ -0,0 +1,23 @@
+﻿using CIR.Core.Entities.GlobalConfig;
+using CIR.Core.Interfaces.GlobalConfig;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIR.Application.Services.GlobalConfig
+{
+	public class GlobalConfigurationFontsServices : IGlobalConfigurationFontsServices
+	{
+		private readonly IGlobalConfigurationFontsRepository _globalConfigurationFontsRepository;
+		public GlobalConfigurationFontsServices(IGlobalConfigurationFontsRepository globalConfigurationFontsRepository)
+		{
+			_globalConfigurationFontsRepository = globalConfigurationFontsRepository;
+		}
+		public async Task<IActionResult> GetGlobalConfigurationFonts()
+		{
+			return await _globalConfigurationFontsRepository.GetGlobalConfigurationFonts();
+		}
+		public async Task<IActionResult> CreateOrUpdateGlobalConfigurationFonts(List<GlobalConfigurationFonts> globalConfigurationFonts)
+		{
+			return await _globalConfigurationFontsRepository.CreateOrUpdateGlobalConfigurationFonts(globalConfigurationFonts);
+		}
+	}
+}
---
+++ CIR.Core/Interfaces/GlobalConfig/IGlobalConfigurationFontsRepository.cs
@@ -0,0 +1,11 @@
+﻿using CIR.Core.Entities.GlobalConfig;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIR.Core.Interfaces.GlobalConfig
+{
+	public interface IGlobalConfigurationFontsRepository
+	{
+		Task<IActionResult> GetGlobalConfigurationFonts();
+		Task<IActionResult> CreateOrUpdateGlobalConfigurationFonts(List<GlobalConfigurationFonts> globalConfigurationFonts);
+	}
+}
