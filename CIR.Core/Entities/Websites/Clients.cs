08ba1a889a9388aa27d2521b59fb01e2151bbe3d
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2023-01-12 18:42:11 +0530

    CIR-87 Clients Backend

---
+++ CIR.Core/Entities/Websites/Clients.cs
@@ -0,0 +1,10 @@
+﻿namespace CIR.Core.Entities.Websites
+{
+    public class Clients
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long SubsiteId { get; set; }
+        public string Code { get; set; }
+    }
+}
