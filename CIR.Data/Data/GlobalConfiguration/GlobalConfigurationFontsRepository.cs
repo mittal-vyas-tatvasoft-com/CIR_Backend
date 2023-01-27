c4e36fb5be8542406058c29588ec5425b2c3e093
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2023-01-27 20:11:35 +0530

    Dapper for global config fonts

--- CIR.Data/Data/GlobalConfiguration/GlobalConfigurationFontsRepository.cs
+++ CIR.Data/Data/GlobalConfiguration/GlobalConfigurationFontsRepository.cs
@@ -3,110 +3,105 @@ using CIR.Common.Enums;
 using CIR.Common.Helper;
 using CIR.Core.Entities.GlobalConfiguration;
 using CIR.Core.Interfaces.GlobalConfiguration;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace CIR.Data.Data.GlobalConfiguration
 {
     public class GlobalConfigurationFontsRepository : ControllerBase, IGlobalConfigurationFontsRepository
-	{
-		#region PROPERTIES
-		private readonly CIRDbContext _CIRDBContext;
-		#endregion
+    {
+        #region PROPERTIES
+        private readonly CIRDbContext _CIRDBContext;
+        #endregion
 
-		#region CONSTRUCTOR
-		public GlobalConfigurationFontsRepository(CIRDbContext context)
-		{
-			_CIRDBContext = context ??
-				throw new ArgumentNullException(nameof(context));
-		}
-		#endregion
+        #region CONSTRUCTOR
+        public GlobalConfigurationFontsRepository(CIRDbContext context)
+        {
+            _CIRDBContext = context ??
+                throw new ArgumentNullException(nameof(context));
+        }
+        #endregion
 
-		#region METHODS
+        #region METHODS
 
-		/// <summary>
-		/// This method is used by get globalconfiguration fonts list
-		/// </summary>
-		/// <returns></returns>
-		public async Task<IActionResult> GetGlobalConfigurationFonts()
-		{
-			try
-			{
-				var fonts = await _CIRDBContext.GlobalConfigurationFonts.Select(x => new GlobalConfigurationFonts()
-				{
-					Id = x.Id,
-					Name = x.Name,
-					Enabled = x.Enabled,
-					FontFamily = x.FontFamily,
-					IsDefault = x.IsDefault
-				}).ToListAsync();
+        /// <summary>
+        /// This method is used by get globalconfiguration fonts list
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> GetGlobalConfigurationFonts()
+        {
+            try
+            {
+                List<GlobalConfigurationFonts> globalConfigurationFontsList;
+                using (DbConnection dbConnection = new DbConnection())
+                {
+                    using (var connection = dbConnection.Connection)
+                    {
+                        globalConfigurationFontsList = connection.Query<GlobalConfigurationFonts>("spGetGlobalConfigurationFonts", null, commandType: CommandType.StoredProcedure).ToList();
+                    }
+                }
 
-				if (fonts.Count == 0)
-				{
-					return new JsonResult(new CustomResponse<List<GlobalConfigurationFonts>>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.NotFound, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.NotFound.GetDescriptionAttribute() });
-				}
-				return new JsonResult(new CustomResponse<List<GlobalConfigurationFonts>>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.Success, Result = true, Message = HttpStatusCodesAndMessages.HttpStatus.Success.GetDescriptionAttribute(), Data = fonts });
-			}
-			catch (Exception ex)
-			{
-				return new JsonResult(new CustomResponse<Exception>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.InternalServerError, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.InternalServerError.GetDescriptionAttribute(), Data = ex });
-			}
-		}
+                if (globalConfigurationFontsList.Count == 0)
+                {
+                    return new JsonResult(new CustomResponse<List<GlobalConfigurationFonts>>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.NotFound, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.NotFound.GetDescriptionAttribute() });
+                }
+                return new JsonResult(new CustomResponse<List<GlobalConfigurationFonts>>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.Success, Result = true, Message = HttpStatusCodesAndMessages.HttpStatus.Success.GetDescriptionAttribute(), Data = globalConfigurationFontsList });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new CustomResponse<Exception>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.InternalServerError, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.InternalServerError.GetDescriptionAttribute(), Data = ex });
+            }
+        }
 
-		/// <summary>
-		/// This method used by create or update globalconfiguration fonts
-		/// </summary>
-		/// <param name="globalConfigurationFonts"></param>
-		/// <returns></returns>
-		public async Task<IActionResult> CreateOrUpdateGlobalConfigurationFonts(List<GlobalConfigurationFonts> globalConfigurationFonts)
-		{
-			try
-			{
-				if (globalConfigurationFonts.Any(x => x.Name == "string" || x.Name == string.Empty))
-				{
-					return new JsonResult(new CustomResponse<string>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.BadRequest, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.BadRequest.GetDescriptionAttribute(), Data = SystemMessages.msgEnterValidData });
-				}
-				if (globalConfigurationFonts != null)
-				{
-					foreach (var item in globalConfigurationFonts)
-					{
-						if (item.Id != 0)
-						{
-							GlobalConfigurationFonts globalConfigFonts = new GlobalConfigurationFonts()
-							{
-								Id = item.Id,
-								Name = item.Name,
-								Enabled = item.Enabled,
-								FontFamily = item.FontFamily,
-								IsDefault = item.IsDefault
-							};
-							_CIRDBContext.GlobalConfigurationFonts.Update(globalConfigFonts);
-						}
-						else
-						{
-							GlobalConfigurationFonts globalConfigFonts = new GlobalConfigurationFonts()
-							{
-								Name = item.Name,
-								Enabled = item.Enabled,
-								FontFamily = item.FontFamily,
-								IsDefault = item.IsDefault
-							};
-							_CIRDBContext.GlobalConfigurationFonts.Add(globalConfigFonts);
+        /// <summary>
+        /// This method used by create or update globalconfiguration fonts
+        /// </summary>
+        /// <param name="globalConfigurationFonts"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> CreateOrUpdateGlobalConfigurationFonts(List<GlobalConfigurationFonts> globalConfigurationFonts)
+        {
+            try
+            {
+                if (globalConfigurationFonts.Any(x => x.Name == "string" || x.Name == string.Empty))
+                {
+                    return new JsonResult(new CustomResponse<string>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.BadRequest, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.BadRequest.GetDescriptionAttribute(), Data = SystemMessages.msgEnterValidData });
+                }
+                if (globalConfigurationFonts != null)
+                {
+                    var result = 0;
+                    foreach (var item in globalConfigurationFonts)
+                    {
+                        using (DbConnection dbConnection = new DbConnection())
+                        {
+                            using (var connection = dbConnection.Connection)
+                            {
+                                DynamicParameters parameters = new DynamicParameters();
+                                parameters.Add("@Id", item.Id);
+                                parameters.Add("@Name", item.Name);
+                                parameters.Add("@FontFamily", item.FontFamily);
+                                parameters.Add("@IsDefault", item.IsDefault);
+                                parameters.Add("@Enabled", item.Enabled);
 
-						}
-					}
-					await _CIRDBContext.SaveChangesAsync();
-					return new JsonResult(new CustomResponse<string>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.Saved, Result = true, Message = HttpStatusCodesAndMessages.HttpStatus.Saved.GetDescriptionAttribute(), Data = string.Format(SystemMessages.msgDataSavedSuccessfully, "Global Configuration Fonts") });
-				}
-				return new JsonResult(new CustomResponse<string>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.BadRequest, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.BadRequest.GetDescriptionAttribute(), Data = SystemMessages.msgBadRequest });
-			}
-			catch (Exception ex)
-			{
-				return new JsonResult(new CustomResponse<Exception>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.InternalServerError, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.InternalServerError.GetDescriptionAttribute(), Data = ex });
-			}
-		}
-		#endregion
+                                result = connection.Execute("spCreateOrUpdateGlobalConfigurationFonts", parameters, commandType: CommandType.StoredProcedure);
+                            }
+                        }
+                    }
+                    if (result != 0)
+                    {
+                        return new JsonResult(new CustomResponse<string>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.Saved, Result = true, Message = HttpStatusCodesAndMessages.HttpStatus.Saved.GetDescriptionAttribute(), Data = string.Format(SystemMessages.msgDataSavedSuccessfully, "GlobalConfiguration Reason") });
+                    }
+                    return new JsonResult(new CustomResponse<Exception>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.UnprocessableEntity, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.UnprocessableEntity.GetDescriptionAttribute() });
+                }
+                return new JsonResult(new CustomResponse<string>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.BadRequest, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.BadRequest.GetDescriptionAttribute(), Data = SystemMessages.msgBadRequest });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new CustomResponse<Exception>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.InternalServerError, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.InternalServerError.GetDescriptionAttribute(), Data = ex });
+            }
+        }
+        #endregion
 
 
-	}
+    }
 }
