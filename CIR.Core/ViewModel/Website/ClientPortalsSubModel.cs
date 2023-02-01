13184d3b7241220694fcddb66a990b09cef14759
BlacyMacwan <blacy.macwan@tatvasoft.com>
2023-02-01 14:18:29 +0530

    Revert "CIR-Merged with main."
    This reverts commit 6288467f5a79b21b6f366e0fa168d3229ce88b00, reversing
    changes made to ba9633d1a438e5b3d8db67a4e3ef3076efda1668.

--- CIR.Core/ViewModel/Website/ClientPortalsSubModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CIR.Core.ViewModel.Website
-{
-    public class ClientPortalsSubModel
-    {
-        public long ClientId { get; set; }
-        public string ClientName { get; set; }
-        public List<PortalSubModel> Portals { get; set; }
-    }
-}
