b723aaaaebc0341aacb37ae20f623d0306bf0e10
ChiragMajethiyaTatvasoft <chirag.majethiya@tatvasoft.com>
2023-02-02 12:35:02 +0530

    Enum Configuration

--- CIR.Common/Enums/GlobalConfigurationEnums.cs
+++ CIR.Common/Enums/GlobalConfigurationEnums.cs
@@ -1,45 +1,54 @@
 ﻿namespace CIR.Common.Enums
 {
-    public class GlobalConfigurationEnums
-    {
-        public enum CutOffDays
-        {
-            SameDay = 0,
+	public class GlobalConfigurationEnums
+	{
+		public enum CutOffDays
+		{
+			SameDay = 0,
 
-            PreviousDay = 1
-        }
+			PreviousDay = 1
+		}
 
-        public enum DynamicDataFields
-        {
-            Reference = 0,
-            BookingId = 1,
-            OrderNumber = 2,
-            CustomerEmail = 3,
-            CustomerName = 4,
-            CollectionDate = 5,
-            CollectionAddress = 6,
-            TrackingURL = 7,
-            LabelURL = 8,
-            BookingURL = 9
-        }
+		public enum DynamicDataFields
+		{
+			Reference = 0,
+			BookingId = 1,
+			OrderNumber = 2,
+			CustomerEmail = 3,
+			CustomerName = 4,
+			CollectionDate = 5,
+			CollectionAddress = 6,
+			TrackingURL = 7,
+			LabelURL = 8,
+			BookingURL = 9
+		}
 
-        public enum StyleOrder
-        {
-            Ascending = 0,
-            Descending = 1
-        }
+		public enum StyleOrder
+		{
+			Ascending = 0,
+			Descending = 1
+		}
 
-        public enum StyleType
-        {
-            Textbox = 0,
-            Colorpicker = 1
-        }
+		public enum StyleType
+		{
+			Textbox = 0,
+			Colorpicker = 1
+		}
 
-        public enum Type
-        {
-            Refund = 1,
-            Return = 2,
-            ProductType = 3
-        }
-    }
+		public enum Type
+		{
+			Refund = 1,
+			Return = 2,
+			ProductType = 3
+		}
+
+		public enum Fields
+		{
+			AddressLine2 = 1,
+			Postcode = 2,
+			MobileNumber = 3,
+			TelePhoneNumber = 4,
+			State = 5
+		}
+	}
 }
--- CIR.Data/Data/GlobalConfiguration/GlobalConfigurationFieldRepository.cs
+++ CIR.Data/Data/GlobalConfiguration/GlobalConfigurationFieldRepository.cs
@@ -3,6 +3,7 @@ using CIR.Common.Enums;
 using CIR.Common.Helper;
 using CIR.Core.Entities.GlobalConfiguration;
 using CIR.Core.Interfaces.GlobalConfiguration;
+using CIR.Core.ViewModel.GlobalConfiguration;
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
@@ -41,11 +42,25 @@ namespace CIR.Data.Data.GlobalConfiguration
 						globalConfigurationField = connection.Query<GlobalConfigurationField>("spGetAllFields", null, commandType: CommandType.StoredProcedure).ToList();
 					}
 				}
-				if (globalConfigurationField.Count == 0)
+
+				List<GlobalConfigurationFieldViewModel> GlobalConfigurationFieldViewModel = new List<GlobalConfigurationFieldViewModel>();
+
+				foreach (var item in globalConfigurationField)
+				{
+					GlobalConfigurationFieldViewModel fieldVm = new GlobalConfigurationFieldViewModel();
+					fieldVm.FieldName = Enum.GetName(typeof(GlobalConfigurationEnums.Fields), item.FieldTypeId);
+					fieldVm.Id = item.Id;
+					fieldVm.FieldTypeId = item.FieldTypeId;
+					fieldVm.Enabled = item.Enabled;
+					fieldVm.Required = item.Required;
+					GlobalConfigurationFieldViewModel.Add(fieldVm);
+				}
+
+				if (GlobalConfigurationFieldViewModel.Count == 0)
 				{
 					return new JsonResult(new CustomResponse<List<GlobalConfigurationField>>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.NotFound, Result = false, Message = HttpStatusCodesAndMessages.HttpStatus.NotFound.GetDescriptionAttribute() });
 				}
-				return new JsonResult(new CustomResponse<List<GlobalConfigurationField>>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.Success, Result = true, Message = HttpStatusCodesAndMessages.HttpStatus.Success.GetDescriptionAttribute(), Data = globalConfigurationField });
+				return new JsonResult(new CustomResponse<List<GlobalConfigurationFieldViewModel>>() { StatusCode = (int)HttpStatusCodesAndMessages.HttpStatus.Success, Result = true, Message = HttpStatusCodesAndMessages.HttpStatus.Success.GetDescriptionAttribute(), Data = GlobalConfigurationFieldViewModel });
 
 			}
 			catch (Exception ex)
